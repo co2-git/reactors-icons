9e323e118afe1af3562084098a759d8073d6214e
co2-git <francoisrvespa@gmail.com>
2016-12-19 00:47:55 -0600

    Use web

--- app/components/Icon.js
+++ app/components/Icon.js
@@ -4,7 +4,18 @@ import IconWeb from './web';
 
 export default function Icon(props) {
   switch (Reactors.platform) {
-  default: throw new Error('Unknown reactors platform ' + Reactors.platform);
-  case 'web': case 'desktop': return <IconWeb {...props} />;
+
+  default: {
+    throw new Error('Unknown reactors platform ' + Reactors.platform);
+  }
+
+  case 'web': case 'desktop': {
+    return <IconWeb {...props} />;
+  }
+
+  case 'mobile': case 'ios': case 'android': {
+    const {Text} = require('react-native');
+    return <Text>MOBILE ICON</Text>;
+  }
   }
 }
--- app/components/web.js
+++ app/components/web.js
@@ -6,25 +6,27 @@ import {Gesture} from 'reactors';
 export default class IconWeb extends Component {
   componentWillMount() {
     if (typeof window === 'object') {
-      const link = document.createElement('link');
-      link.rel = 'stylesheet';
-      link.href = Icon.href;
-      document.querySelector('head').appendChild(link);
+      if (Icon.href) {
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = Icon.href;
+        document.querySelector('head').appendChild(link);
+      } else {
+        console.warn('reactors-icons: no href set');
+      }
     }
   }
   render() {
     const className = ['fa', `fa-${this.props.name}`, this.props.className];
-    const handlers = Gesture.handlers({
-      onPress: (event) => {
-        if (_.isFunction(this.props.onPress)) {
-          this.props.onPress(event);
-        }
-      },
-    });
-    return <span
-      className={className.join(' ')}
-      {..._.omit(this.props, ['vendor', 'name', 'size', 'onPress'])}
-      {...handlers}
-      />;
+    const handlers = Gesture.handlers(this.props);
+    const props = _.omit({...handlers}, [
+      'vendor', 'name', 'size'
+    ]);
+    return (
+      <span
+        className={className.join(' ')}
+        {...props}
+        />
+    );
   }
 }
